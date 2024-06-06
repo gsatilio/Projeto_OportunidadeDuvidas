5d1701bc42d086666eae2654dbceae26fb0eb935
Guilherme <g.satilio@gmail.com>
2024-06-06 19:30:13 -0300

    Add project files.

---
+++ APIDuvidas/Data/APIDuvidasContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Models;
+
+namespace APIDuvidas.Data
+{
+    public class APIDuvidasContext : DbContext
+    {
+        public APIDuvidasContext (DbContextOptions<APIDuvidasContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Models.Duvidas> Duvidas { get; set; } = default!;
+    }
+}
---
+++ APIOportunidade/Data/APIOportunidadeContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Models;
+
+namespace APIOportunidade.Data
+{
+    public class APIOportunidadeContext : DbContext
+    {
+        public APIOportunidadeContext (DbContextOptions<APIOportunidadeContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Models.Oportunidade> Oportunidade { get; set; } = default!;
+    }
+}
